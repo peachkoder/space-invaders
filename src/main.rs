17ac60016e3098de4812209530e3ae65e64948ec
peachkoder <peachkoder@gmail.org>
2022-11-11 10:42:22 +0000

    initial

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{terminal, ExecutableCommand};
+use rusty_audio::Audio;
+use std::error::Error;
+use std::{io, thread};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Cleanup
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
